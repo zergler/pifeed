527c8be6ba72c42ab3d40f121615a1061bf09cfc
Igor Janjic <i.janjic@live.com>
2014-11-26 11:34:33 -0500

    Added template for functions to configure PMOD

--- src/pmod.py
+++ src/pmod.py
@@ -15,32 +15,142 @@ class PMOD:
         self.ESC = 0x1B                 # ^[
         self.BRACKET = 0x5B             # [
         self.RESET = 0x2A               # *: reset (cycles power)
-        self.CURSOR_SET = 0x48          # H: set cursor position
-        self.CURSOR_SAVE = 0x73         # s: save cursor position
-        self.CURSOR_MODE = 0x63         # c: set cursor mode
-        self.CURSOR_RESTORE = 0x75      # u: restore saved cursor position
+        self.CURSOR_POS_SET = 0x48      # H: set cursor position
+        self.CURSOR_POS_SAVE = 0x73     # s: save cursor position
+        self.CURSOR_POS_RESTORE = 0x75  # u: restore saved cursor position
+        self.CURSOR_MODE_SET = 0x63     # c: set cursor mode
+        self.CURSOR_MODE_SAVE = 0x6E    # n: save cursor mode to EEPROM
         self.ERASE_INLINE = 0x4B        # K: erase within line
         self.ERASE_FIELD = 0x4E         # N: erase field in current line
         self.SCROLL_LEFT = 0x40         # @: scroll left
         self.SCROLL_RIGHT = 0x41        # A: scroll right
         self.DISP_EN = 0x65             # e: enable backlight for dislay
         self.DISP_CLEAR = 0x6A          # j: clear display and home cursor
-        self.DISP_MODE = 0x68           # h: set display mode
+        self.DISP_MODE_SET = 0x68       # h: set display mode
         self.DISP_MODE_SAVE = 0x6F      # o: save display mode to EEPROM
         self.BAUD_RATE_SAVE = 0x62      # b: save baud rate value in EEPROM
-        self.CHAR_PRGM = 0x70           # p: program character table into LCD
-        self.CHAR_SAVE = 0x74           # t: save RAM character table to EEPROM
-        self.CHAR_LOAD = 0x6C           # l: load EEPROM character table to RAM
+        self.TABLE_PRGM = 0x70          # p: program character table into LCD
+        self.TABLE_SAVE = 0x74          # t: save RAM character table to EEPROM
+        self.TABLE_LOAD = 0x6C          # l: load EEPROM character table to RAM
         self.CHAR_DEF = 0x64            # d: define user programmable character
         self.COMM_MODE_SAVE = 0x6D      # m: save communication mode to EEPROM
         self.TWI_ADDR_SAVE = 0x61       # a: save TWI address in EEPROM
         self.EEPROM_WRITE_EN = 0x77     # w: enable write to EEPROM
-        self.CURSOR_MODE_SAVE = 0x6E    # n: save cursor mode to EEPROM
 
         # Parameters for communication ports.
         self.PAR_ACCESS_DSPI0 = 0
         self.PAR_ACCESS_DSPI1 = 1
         self.PAR_SPD_MAX = 625000
+
+    def begin(self, accessType):
+        """ Initializes and configures the communication interface.
+        """
+        pass
+
+    def cursorPosSet(self, row, col):
+        """ Sets the cursor position.
+        """
+        pass
+
+    def cursorPosSave(self):
+        """ Saves the current cursor position.
+        """
+        pass
+
+    def cursorPosRestore(self):
+        """ Restores the saved cursor position.
+        """
+        pass
+
+    def cursorModeSet(self, mode):
+        """ Sets the cursor mode.
+        """
+        pass
+
+    def cursorModeSave(self, mode):
+        """ Saves the cursor mode to EEPROM.
+        """
+        pass
+
+    def eraseInline(self, mode):
+        """ Erases within line based on the mode.
+
+            If mode is 0, erases from the current position to end of the line.
+            If mode is 1, erases from the start of the line to the current
+            position. If mode is 2, erases the entire line.
+        """
+        pass
+
+    def eraseField(self, chars):
+        """ Erases field in the current line where chars is the number of chars
+            to erase starting at the current position.
+        """
+        pass
+
+    def scrollLeft(self, cols):
+        """ Scrolls left by the selected number of columns.
+        """
+        pass
+
+    def scrollRight(self, cols):
+        """ Scrolls right by the selected number of columns.
+        """
+        pass
+
+    def dispClear(self):
+        """ Clears the display and home the cursor.
+        """
+        pass
+
+    def dispModeSet(self, mode):
+        """ Sets the display mode.
+        """
+        pass
+
+    def dispModeSave(self, mode):
+        """ Saves the display mode to EEPROM.
+        """
+        pass
+
+    def baudRateSave(self, baud):
+        """ Saves the baud rate to EEPROM.
+        """
+        pass
+
+    def tablePrgrm(self, table):
+        """ Programs the character table into the LCD.
+        """
+        pass
+
+    def tableSave(self, table):
+        """ Saves the RAM character table to EEPROM.
+        """
+        pass
+
+    def tableLoad(self, table):
+        """ Loads the EEPROM character table to RAM.
+        """
+        pass
+
+    def charDef(self, char, pos):
+        """ Defines a user programmable character.
+        """
+        pass
+
+    def commModeSave(self, mode):
+        """ Saves the communication mode to EEPROM.
+        """
+        pass
+
+    def twiAddrSave(self, addr):
+        """ Saves the TWI address to EEPROM.
+        """
+        pass
+
+    def eepromWriteEn(self):
+        """ Enables writign to EEPROM.
+        """
+        pass
 
 
 class ErrorPMOD(Exception):
